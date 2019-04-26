0264283b4db724a039c3f7a6b9bcb4955afb204e
ngosavi <ngosavi@XIPL0644.xpanxion.co.in>
2019-04-26 17:44:27 +0530

    added logs api and other things

--- src/main/java/com/xpx/controllers/EmployeeController.java
+++ src/main/java/com/xpx/controllers/EmployeeController.java
@@ -1,16 +1,24 @@
 package com.xpx.controllers;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
 import com.xpx.dao.EmployeeRepository;
 import com.xpx.entity.Employee;
 
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
@@ -18,7 +26,19 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository empRepo;
 
-	// Creates and Returns an Employee object if not registered already (with email) Or returns with response code Not Acceptable.
+	// returns all registered employees
+	@GetMapping
+	public ResponseEntity<List<Employee>> getAllEmployees() {
+
+		List<Employee> empList = new ArrayList<>();
+		Iterable<Employee> empItr = empRepo.findAll();
+		empItr.forEach((e) -> empList.add(e));
+		System.out.println("EmpList" + empList);
+		return new ResponseEntity<List<Employee>>(empList, HttpStatus.OK);
+	}
+
+	// Creates and Returns an Employee object if not registered already (with
+	// email) Or returns with response code Not Acceptable.
 
 	@PostMapping
 	public ResponseEntity<Employee> createEmployee(@RequestBody Employee emp) {
@@ -27,11 +47,18 @@ public class EmployeeController {
 			System.out.println("Employee created" + emp);
 			Employee employee = empRepo.save(emp);
 			ResponseEntity<Employee> res = new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
-
 			return res;
 		} else {
 			return new ResponseEntity<Employee>(HttpStatus.NOT_ACCEPTABLE);
 		}
 
+	}
+
+	@GetMapping("/info")
+	public ResponseEntity<Employee> userInfo(@SessionAttribute("employee") Employee employee) {
+
+		ResponseEntity<Employee> res = new ResponseEntity<Employee>(employee, HttpStatus.OK);
+
+		return res;
 	}
 }
--- src/main/java/com/xpx/controllers/LoginController.java
+++ src/main/java/com/xpx/controllers/LoginController.java
@@ -2,30 +2,35 @@ package com.xpx.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.xpx.dao.EmployeeRepository;
 import com.xpx.entity.Employee;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/login")
+@SessionAttributes("employee")
 public class LoginController {
 
 	@Autowired
-	EmployeeRepository empRepo;
-
-	
+	EmployeeRepository empRepo;	
 	
 	// Returns an Employee object if authenticated Or returns with response code not found.
 	 
 	@PostMapping
-	public ResponseEntity<Employee> authenticate(@RequestBody Employee employee) {
+	public ResponseEntity<Employee> authenticate(HttpServletRequest request ,@RequestBody Employee employee) {
 
 		Employee emp = empRepo.findByEmail(employee.getEmail());
 
@@ -34,7 +39,11 @@ public class LoginController {
 		} else if (!emp.getPassword().equals(employee.getPassword())) {
 			return new ResponseEntity<Employee>(HttpStatus.NOT_FOUND);
 		} else {
-			return new ResponseEntity<Employee>(emp, HttpStatus.FOUND);
+			//sessionEmp =emp;
+			request.getSession().setAttribute("employee", emp);
+			return new ResponseEntity<Employee>(emp, HttpStatus.OK);		
+			
+			
 		}
 	}
 
